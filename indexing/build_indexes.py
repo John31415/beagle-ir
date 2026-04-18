466c2e0d1fd2a8b9acede32cd9bc33df4bc3e76b
John31415 <johnmauris04@gmail.com>
2026-04-18 19:45:42 -0400

    refactor(indexing): organize utils

--- indexing/build_indexes.py
+++ indexing/build_indexes.py
@@ -1,8 +1,8 @@
 from indexing.index_bm25f import IndexBM25F
 from utils.files import read_urls_corpus
 from utils.hash import hash_str
-from indexing.utils.document_parser import ExtractPdf
-from indexing.utils.text_processor import chunking, TextPreprocessor
+from utils.document_parser import ExtractPdf
+from utils.text_processor import chunking, TextPreprocessor
 from indexing.dense_index import DenseIndexer
 
 def build_indexes():
