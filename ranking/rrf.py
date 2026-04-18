f7c1fb0d03e4cca4eb1332772bc19d63004d0229
John31415 <johnmauris04@gmail.com>
2026-04-18 19:24:38 -0400

    feat(ranking): modify rrf to admit multiple rankings as parameters

--- ranking/rrf.py
+++ ranking/rrf.py
@@ -1,13 +1,12 @@
 from collections import defaultdict
 
-def rrf_fusion(list_bm25: list[str], list_dense: list[str], k = 60) -> list[str]:
+def rrf_fusion(lists: list[list[str]], k = 60) -> list[str]:
     """Reciprocal Rank Funsion for combining search results
     """
 
     scores = defaultdict(float)
-    for rank, doc_id in enumerate(list_bm25):
-        scores[doc_id] += 1.0 / (k + rank + 1)
-    for rank, doc_id in enumerate(list_dense):
-        scores[doc_id] += 1.0 / (k + rank + 1)
+    for i in range(len(lists)):
+        for rank, doc_id in enumerate(lists[i]):
+            scores[doc_id] += 1.0 / (k + rank + 1)
     ranked_docs = sorted(scores.items(), key = lambda x: x[1], reverse = True)
     return [doc_id for doc_id, _ in ranked_docs]
