374efe8863374ab020a13a5a6965988a915befea
John31415 <johnmauris04@gmail.com>
2026-04-18 17:13:32 -0400

    feat(indexing): build index for dense embeddings

--- indexing/build_indexes.py
+++ indexing/build_indexes.py
@@ -1,8 +1,9 @@
 from indexing.index_bm25f import IndexBM25F
 from utils.files import read_urls_corpus
 from utils.hash import hash_str
-from indexing.utils.document_parser import extract_pdf
+from indexing.utils.document_parser import ExtractPdf
 from indexing.utils.text_processor import chunking, TextPreprocessor
+from indexing.dense_index import DenseIndexer
 
 def build_indexes():
     """Build BM25F index and (embeddings...)
@@ -11,24 +12,32 @@ def build_indexes():
     corpus_urls_path = "downloaded_urls.txt"
     urls = read_urls_corpus(corpus_urls_path)
     index_bm25f = IndexBM25F()
+    dense_index = DenseIndexer()
     for i_, url in enumerate(urls):
         print(i_)
         url_hash = hash_str(url)
         pdf_path = "corpus/" + url_hash + ".pdf"
-        pdf_fields = extract_pdf(pdf_path)
+        ext_pdf = ExtractPdf(pdf_path)
+        pdf_fields = ext_pdf.extract_pdf()
         text_processor = TextPreprocessor(pdf_fields['text'])
         tokens = text_processor.get_normalized_tokens()
         chunks = chunking(tokens)
         for (i, chunk) in enumerate(chunks):
             chunk_hash = hash_str(url_hash + str(i))
-            chunk_fields = {
+            text = " ".join(chunk)
+            index_bm25f.add_document({
                 'id': chunk_hash,
                 'title': pdf_fields['title'],
                 'authors': pdf_fields['authors'],
                 'abstract': pdf_fields['abstract'],
-                'text': " ".join(chunk),
+                'text': text,
                 'pdf_hash': url_hash
-            }
-            index_bm25f.add_document(chunk_fields)
+            })
+            dense_index.add_chunk({
+                'text': text,
+                'pdf_hash': url_hash,
+                'chunk_hash': chunk_hash
+            })
+    dense_index.save()
 
 build_indexes()
