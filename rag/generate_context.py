74c044b7a732d6246e86d747b1ea0debfc679285
John31415 <johnmauris04@gmail.com>
2026-04-19 00:53:58 -0400

    feat(rag): add function to generate context for a given query by retrieving information

---
+++ rag/generate_context.py
@@ -0,0 +1,15 @@
+from ranking.ranking import Ranker
+from indexing.persist_chunks import PersistChunk
+
+def get_chunks_context(query: str) -> list[dict]:
+    ranker = Ranker(query)
+    chunks_rank = ranker.chunk_ranker()
+    context = []
+    persist_chunk = PersistChunk()
+    for chunk in chunks_rank:
+        chunk_data = persist_chunk.get_chunk_by_hash(chunk)
+        context.append({
+            'title': chunk_data['title'],
+            'content': chunk_data['content']
+        })
+    return context
