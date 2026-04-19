25bb8c8819bcbd8ddbf72d0c0eb42a7d1200acd2
John31415 <johnmauris04@gmail.com>
2026-04-18 20:10:36 -0400

    feat(web_search): add function to request for files matching the querys and download them

---
+++ web_search/web_search.py
@@ -0,0 +1,27 @@
+from web_search.arxiv_api_search import search_arxiv
+from utils.text_processor import TextPreprocessor
+from data_ingestion.utils.files import store_url_corpus
+from data_ingestion.utils.urls import download_file
+from data_ingestion.utils.robots import RobotsParser
+from utils.hash import hash_str
+
+def _download(pdfs_urls: list[str], crawl_delay: int):
+    for url in pdfs_urls:
+        store_url_corpus(url, "corpus_urls.txt")
+        if download_file("corpus", url, crawl_delay):
+            store_url_corpus(url, "downloaded_urls.txt")
+
+def web_search(query: str) -> list[str]:
+    """Method for finding articles that match the query on the web.
+    """
+
+    robots_parser = RobotsParser("https://arxiv.org/robots.txt")
+    robots_parser.parse_robots_txt()
+    crawl_delay = robots_parser.get_delay()
+    tp = TextPreprocessor(query)
+    query_tokens = tp.get_normalized_tokens()
+    query_processed = " ".join(query_tokens)
+    pdfs_urls = search_arxiv(query_processed)
+    _download(pdfs_urls, crawl_delay)
+    pdfs_hash = [hash_str(url) for url in pdfs_urls]
+    return pdfs_hash
