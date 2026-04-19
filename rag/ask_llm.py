f60fd374fc7525f15d5f643642d067caed557cc3
John31415 <johnmauris04@gmail.com>
2026-04-19 00:55:37 -0400

    feat(rag): add function to ask a groq model or a hugging face model

---
+++ rag/ask_llm.py
@@ -0,0 +1,33 @@
+from rag.generate_context import get_chunks_context
+from rag.prompt import create_prompt
+import os
+from groq import Groq
+from huggingface_hub import InferenceClient
+from dotenv import load_dotenv
+
+def ask_llm(query: str) -> str:
+    """Ask an LLLM the user's query with the context retrieved by the system.
+    """
+
+    load_dotenv()
+    context = get_chunks_context(query)
+    prompt = create_prompt(query, context)
+    try:
+        groq_client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+        response = groq_client.chat.completions.create(
+            model = "llama3-8b-8192",
+            messages = [{"role": "user", "content": prompt}],
+            max_completion_tokens = 512,
+            temperature = 0.2,
+        )
+        return response.choices[0].message.content.strip()
+    except Exception as e:
+        print(f"Groq failed with error: {e}.")
+        hf_client = InferenceClient(token=os.getenv("HF_TOKEN"))
+        response = hf_client.chat.completions.create(
+            model = "mistralai/Mistral-7B-Instruct-v0.3",
+            messages = [{"role": "user", "content": prompt}],
+            max_tokens = 512,
+            temperature = 0.2,
+        )
+        return response.choices[0].message.content.strip()
