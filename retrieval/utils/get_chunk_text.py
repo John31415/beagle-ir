e314227ea950f63f0380d0762e5ec6bf45871c11
John31415 <johnmauris04@gmail.com>
2026-04-18 20:41:53 -0400

    fix(rettrieval): fix get chunk text

--- retrieval/utils/get_chunk_text.py
+++ retrieval/utils/get_chunk_text.py
@@ -1,6 +1,5 @@
 from whoosh.index import open_dir
 import os
-from whoosh.qparser import MultifieldParser
 
 def get_chunk_text(chunk_hash: str) -> str:
     """Returns the text of the chunk.
@@ -9,10 +8,6 @@ def get_chunk_text(chunk_hash: str) -> str:
     index_path = os.path.join("indexing", "index_dir")
     ix = open_dir(index_path)
     with ix.searcher() as searcher:
-        qp = MultifieldParser(
-            ["chunk_hash"],
-            schema=ix.schema
-        )
-        q = qp.parse(chunk_hash)
-        results = searcher.search(q, limit=1)
-        return results[0]["content"]
+        results = searcher.document(id = chunk_hash)
+        print(results)
+        return results["content"]
