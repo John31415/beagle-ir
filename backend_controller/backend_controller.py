72c830d904f5848d4e0eef81071c8205ea94c6f4
John31415 <johnmauris04@gmail.com>
2026-04-20 00:29:06 -0400

    feat(backend_controller): add recommendations support

--- backend_controller/backend_controller.py
+++ backend_controller/backend_controller.py
@@ -1,16 +1,24 @@
 from ranking.ranking import Ranker
 from rag.ask_llm import ask_llm
 from query_expansion.query_expander import PRFExpander
+from recommender.recommender import recommender
+from recommender.utils.persist_history import add_query
+from datetime import datetime
 
 def retrieval_controller(query: str) -> list[str]:
+    add_query(query, datetime.now())
     expander = PRFExpander()
     expanded_query = expander.expand(query)
     ranker = Ranker(query, expanded_query)
     pdfs = ranker.pdf_ranker()
-    return ["corpus" + pdf + ".pdf" for pdf in pdfs]
+    return ["corpus/" + pdf + ".pdf" for pdf in pdfs]
 
 def rag_controller(query: str) -> str:
     expander = PRFExpander()
     expanded_query = expander.expand(query)
     llm_answer = ask_llm(query, expanded_query)
     return llm_answer
+
+def get_recommendations() -> list[str]:
+    pdfs = recommender()
+    return ["corpus/" + pdf + ".pdf" for pdf in pdfs]
