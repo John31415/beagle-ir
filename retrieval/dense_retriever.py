754539d1130c6cfc93d6f18d682cb21bfb48ce9e
John31415 <johnmauris04@gmail.com>
2026-04-22 03:53:00 -0400

    feat(retrieval): modify dense_retriever to allow PDFs filtering based on the algorithm score and a threshold

--- retrieval/dense_retriever.py
+++ retrieval/dense_retriever.py
@@ -1,28 +1,26 @@
-from sentence_transformers import SentenceTransformer
 import faiss
 import os
 import pickle
-from utils.text_processor import TextPreprocessor 
+from retrieval.utils.dense_utils import build_query_vector
 
-def dense_retriever(query: str, top_n_chunks = 500) -> list[tuple[str, str]]:
+def dense_retriever(query: str, vector = None, top_n_chunks = 100, chunk_similarity_threshold = 0.5) -> list[tuple[str, str]]:
     """Search chunks that semantically match the query
 
     Returns:
         list[str]: list of (chunk_hash, pdf_hash)
     """
 
-    model_name = 'multi-qa-mpnet-base-dot-v1'
     save_dir = "indexing/dense_index"
-    model = SentenceTransformer(model_name)
     index = faiss.read_index(os.path.join(save_dir, "vector_index.faiss"))
     metadata = None
     with open(os.path.join(save_dir, "metadata.pkl"), "rb") as f:
         metadata = pickle.load(f)
-    tp = TextPreprocessor(query)
-    tokens = tp.get_normalized_tokens()
-    query_processed = " ".join(tokens)
-    query_vector = model.encode([query_processed], convert_to_numpy = True)
-    faiss.normalize_L2(query_vector)
-    D, I = index.search(query_vector.astype('float32'), top_n_chunks)
-    ranking = [(metadata[idx]['chunk_hash'], metadata[idx]['pdf_hash']) for idx in I[0] if idx != -1]
-    return ranking
+    query_vector = build_query_vector(query, vector)
+    D, I = index.search(query_vector, top_n_chunks)
+    chunk_candidates= []
+    for score, idx in zip(D[0], I[0]):
+        if idx != -1 and float(score) >= chunk_similarity_threshold:
+            chunk_candidates.append((metadata[idx]["chunk_hash"], metadata[idx]["pdf_hash"], score))
+    filtered = [(chunk_hash, pdf_hash, score) for (chunk_hash, pdf_hash, score) in chunk_candidates]
+    filtered.sort(key = lambda x: x[2], reverse = True)
+    return [(chunk_hash, pdf_hash) for (chunk_hash, pdf_hash, _) in filtered[:top_n_chunks]]
