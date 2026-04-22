bb8c3c6ff5392b745137c20451a10e32e68c6a0b
John31415 <johnmauris04@gmail.com>
2026-04-22 04:24:15 -0400

    feat(backend_controller): add query expansion based on embeddings

--- backend_controller/backend_controller.py
+++ backend_controller/backend_controller.py
@@ -1,6 +1,6 @@
 from ranking.ranking import Ranker
 from rag.ask_llm import ask_llm
-from query_expansion.query_expander import PRFExpander
+from query_expansion.expand_query_dense import expand_query_dense
 from recommender.recommender import recommender
 from recommender.utils.persist_history import add_query
 from datetime import datetime
@@ -10,8 +10,7 @@ def retrieval_controller(query: str) -> list[str]:
     if not cleaned_query:
         return []
     add_query(cleaned_query, datetime.now())
-    expander = PRFExpander()
-    expanded_query = expander.expand(cleaned_query)
+    expanded_query = expand_query_dense(query)
     ranker = Ranker(cleaned_query, expanded_query)
     pdfs = ranker.pdf_ranker()
     return ["corpus/" + pdf + ".pdf" for pdf in pdfs]
@@ -21,8 +20,7 @@ def rag_controller(query: str) -> str:
     if not cleaned_query:
         return ""
     add_query(cleaned_query, datetime.now())
-    expander = PRFExpander()
-    expanded_query = expander.expand(cleaned_query)
+    expanded_query = expand_query_dense(query)
     llm_answer = ask_llm(cleaned_query, expanded_query)
     return llm_answer
 
