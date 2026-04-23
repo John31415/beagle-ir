6a07b859cb2a2ce1045958f21412dd0b830b240c
John31415 <johnmauris04@gmail.com>
2026-04-22 20:19:48 -0400

    feat(rag): modify ask_llm to use cloudflare's api key

--- rag/ask_llm.py
+++ rag/ask_llm.py
@@ -1,29 +1,27 @@
 from rag.generate_context import get_chunks_context
 from rag.prompt import create_prompt
 import os
-from huggingface_hub import InferenceClient
 from dotenv import load_dotenv
 from pathlib import Path
+import os
+from cloudflare import Cloudflare
 
 def ask_llm(query: str, expanded_query: str) -> str:
     env_path = Path(__file__).resolve().parent.parent / ".env"
     load_dotenv(dotenv_path = env_path)
     context = get_chunks_context(query, expanded_query)
-    prompt = create_prompt(query, context)
-    client = InferenceClient(model = "Qwen/Qwen2.5-7B-Instruct", token = os.getenv("HF_TOKEN"))
-    try:
-        response = client.chat_completion(
-            messages = [
-                {
-                    "role": "user",
-                    "content": prompt
-                }
-            ],
-            max_tokens = 512,
-            temperature = 0.2,
-        )
-        return response.choices[0].message.content.strip()
-    except:
-        return "The system is experiencing some problems, please try again later"
-    
-print(ask_llm("what is quantum mechanics", "quantum quantum quantum mechanics mechanics mechanics physics"))
+    (prompt_system, prompt_user) = create_prompt(query, context)
+    client = Cloudflare(
+        api_token=os.environ.get("CF_TOKEN"),
+    )
+    account_id = os.environ.get("CF_ID")
+    response = client.ai.run(
+        account_id=account_id,
+        model_name="@cf/meta/llama-3.1-8b-instruct",
+        messages=[
+            {"role": "system", "content": prompt_system},
+            {"role": "user", "content": prompt_user}
+        ],
+        max_tokens=512,
+    )
+    return response['response']
