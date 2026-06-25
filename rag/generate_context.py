61dd7bb34814ecf8aff0f4ee95d382da2f9ee2d6
John31415 <johnmauris04@gmail.com>
2026-06-25 10:04:52 -0400

    fix(rag): fix ranker call

--- rag/generate_context.py
+++ rag/generate_context.py
@@ -1,16 +1,21 @@
 from ranking.ranking import Ranker
 from indexing.persist_chunks import PersistChunk
 
-def get_chunks_context(query: str, expanded_query: str) -> list[dict]:
+
+def get_chunks_context(
+    query: str, expanded_query: str, top_k_docs: int = 10
+) -> list[dict]:
     ranker = Ranker(query, expanded_query)
-    chunks_rank = ranker.chunk_ranker()[:10]
+    chunks_rank = ranker.chunk_ranker()[:top_k_docs]
     context = []
     persist_chunk = PersistChunk()
-    for chunk in chunks_rank:
+    for chunk, _ in chunks_rank:
         chunk_data = persist_chunk.get_chunk_by_hash(chunk)
-        context.append({
-            'title': chunk_data['title'],
-            'content': chunk_data['content'],
-            'pdf_hash': chunk_data['pdf_hash']
-        })
+        context.append(
+            {
+                "title": chunk_data["title"],
+                "content": chunk_data["content"],
+                "pdf_hash": chunk_data["pdf_hash"],
+            }
+        )
     return context
